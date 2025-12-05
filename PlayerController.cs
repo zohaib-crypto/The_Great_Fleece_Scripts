2e13d81d37a1139f0de292c3b9f37209993461e6
zohaib-crypto <khanzaibi191@gmail.com>
2025-12-05 23:44:35 +0000

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,42 +1,42 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
-// public class PlayerController : MonoBehaviour
-// {
-//     private Animator _anim;
-//     private Vector3 _target;
-//     private NavMeshAgent _agent;
-//     float _distance;
-//     void Start()
-//     {
-//         _agent = GetComponent<NavMeshAgent>();
-//         _anim = GetComponentInChildren<Animator>();
-//     }
+public class PlayerController : MonoBehaviour
+{
+    private Animator _anim;
+    private Vector3 _target;
+    private NavMeshAgent _agent;
+    float _distance;
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponentInChildren<Animator>();
+    }
 
 
-//     void Update()
-//     {
-//         if (Input.GetMouseButtonDown(0))
-//         {
-//             Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
-//             RaycastHit hitInfo;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
 
-//             if (Physics.Raycast(rayOrigin, out hitInfo))
-//             {
-//                 //moving our player to the point where we are clicking
-//                 _agent.SetDestination(hitInfo.point);
-//                 _anim.SetBool("isWalking", true);
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                //moving our player to the point where we are clicking
+                _agent.SetDestination(hitInfo.point);
+                _anim.SetBool("isWalking", true);
 
-//             }
+            }
 
-//         }
-//         if (!_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
-//         {
-//             _anim.SetBool("isWalking", false);
+        }
+        if (!_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            _anim.SetBool("isWalking", false);
 
-//         }
-//     }
-// }
+        }
+    }
+}
 
