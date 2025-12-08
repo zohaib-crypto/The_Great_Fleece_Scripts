a501ac9151059aa2c5eb48f57f1dc9b9b330ace6
zohaib-crypto <khanzaibi191@gmail.com>
2025-12-08 00:38:23 +0000

    making script for guards

---
+++ GuardAI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardAI : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
