0bd04a3bf54dc7ba06c3a1dd73138ae70abe10aa
zohaib-crypto <khanzaibi191@gmail.com>
2025-12-09 09:25:45 +0000

    Update GuardAI.cs

--- GuardAI.cs
+++ GuardAI.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
+    public List<Transform> wayPoints;
+    public Transform currentTarget;
+    private NavMeshAgent _agent;
     void Start()
     {
-
+        _agent = GetComponent<NavMeshAgent>();
+        if (wayPoints.Count > 0)
+        {
+            if (wayPoints[0] != null)
+            {
+                currentTarget = wayPoints[0];
+                _agent.SetDestination(currentTarget.position);
+            }
+        }
     }
     void Update()
     {
